19c5d9f3323018722c0fa1b894510f7b5a91f307
biggs5878 <geraldhooper165@gmail.com>
2021-02-24 07:20:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Welcome
+console.log("Here's the food on the menu:" + food)
+console.log("Here's the drinks we have on the menu:" + drink)
+// the start shows off the menu and a welcome at the bottom
+const food = ['Burgers $5.00' , 'Fries $3.00' , 'Hot Wings $6.00']; //food and price
+const drink = ['Vess Soda $1.00' , 'Shake $4.00']; //drinks and prices
+const Burgers = 1;
+const Fries = 3;
+const Wings = 6;// an idea but might be a better way to work this out better
+const total = math.random(food + drink)
+const Welcome = () => {
+    console.log("Welcome! What would you like to order?")
+}; // was curious if this was better than just having a console log but a variable + identifier
+
+if (1) {
+    math.random(5.00 +(Burgers));
+    print:"5.00$"
+} else if (3)  
+ {
+    math.random(3.00 + (Fries));
+    print:"3.00$"
+ }
+ else if (6)
+{
+    math.random(6.00 +(Wings));
+    print:"6.00$"
+} //if the user chooses between 1,3 or 6 it should do the right amount and add if they add another 
+
+
+food.forEach(food => console.log('We have food items:' + food )); //display food items
+drink.forEach(drink => console.log('We have these drinks:' + drink )); //display drink items
+// the other way i wanted to display the menu im not sure which is more better but love to always give more options!
+
+//function newFunction() {
+        //console.log('Thank you' + food + drink + "here's what you ordered"); //introduce user");
+       // Math.random(total = (food+drink)
+    //}
+    //input
+    // was going try to use this for the receipt and the bottom part for user input
+    // though didnt work as intended and below the receipt part was going to work on the tips one
